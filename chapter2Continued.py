a3076b8d40ad4c8b93d59bbdb6e43b420bb4bb33
harry.morgan13@outlook.com <harry.morgan13@outlook.com>
2022-01-18 15:47:13 +0000

    Added 'personal message'

---
+++ chapter2Continued.py
@@ -0,0 +1,22 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")
+
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+full_name = "{}{}".format(first_name, last_name)
+
+print("Python")
+print("\tPython")
+
+favorite_language = ' python '
+favorite_language.rstrip()
+favorite_language.lstrip()
+favorite_language.strip()
+
+first_name = "william"
+last_name = "Smith"
+print(f"Hello {first_name.title()}, would you like to learn some python today?")
+print(f"Hello {first_name.title()} {last_name.title()}, would you like to learn some python")
